5fd04b5f83e5f036b1d87cd1c9fd138414af8c88
Ashwinjauhary <ashwin2431333@gmail.com>
2025-10-14 16:29:29 +0530

    Fix TypeScript errors in Team.tsx for social media links

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -358,9 +358,9 @@ const Team = () => {
                       >
                         <Linkedin className="w-3 h-3 md:w-4 md:h-4" />
                       </motion.a>
-                      {member.social.twitter && (
+                      {'twitter' in member.social && member.social.twitter && (
                         <motion.a
-                          href={member.social.twitter}
+                          href={member.social['twitter' as keyof typeof member.social] as string}
                           whileHover={{ scale: 1.1, rotate: 5 }}
                           className="p-2 bg-gradient-to-r from-white/10 to-white/5 rounded-lg text-white/60 hover:text-white hover:bg-gradient-to-r hover:from-white/15 hover:to-white/10 transition-all duration-300 border border-white/20 hover:border-catalyst-red/30"
                         >
